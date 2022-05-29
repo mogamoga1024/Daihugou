149b23ced3611b9ce75cb887a546cb28eb0c612f
mogamoga <mogamoga.g1thu6@gmail.com>
2022-05-30 07:11:05 +0900

    カード 画像

---
+++ js/card.js
@@ -0,0 +1,18 @@
+
+class Card {
+    suit = Suit.None;
+    number = 0;
+    power = 0;
+    imagePath = "";
+
+    constructor(suit, number, power, imagePath) {
+        this.suit = suit;
+        this.number = number;
+        this.power = power;
+        this.imagePath = imagePath;
+    }
+
+    toString() {
+
+    }
+}
