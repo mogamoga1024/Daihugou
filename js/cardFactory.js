6eaf9fe6a7ac6cce1cc1eb326e4dc52356789875
mogamoga <mogamoga.g1thu6@gmail.com>
2022-05-30 10:00:25 +0900

    カードの強さ修正

--- js/cardFactory.js
+++ js/cardFactory.js
@@ -23,11 +23,11 @@ class CardFactory {
             this.#cardList.push({name: `${initial}8`, obj: new Card(suit.obj, "8",  `${initial}8`, 6,  `${imageFolderPath}/${suit.name}/card_${suit.name}_8.png`) });
             this.#cardList.push({name: `${initial}9`, obj: new Card(suit.obj, "9",  `${initial}9`, 7,  `${imageFolderPath}/${suit.name}/card_${suit.name}_9.png`) });
             this.#cardList.push({name: `${initial}T`, obj: new Card(suit.obj, "T",  `${initial}T`, 8,  `${imageFolderPath}/${suit.name}/card_${suit.name}_10.png`)});
-            this.#cardList.push({name: `${initial}1`, obj: new Card(suit.obj, "1",  `${initial}1`, 9,  `${imageFolderPath}/${suit.name}/card_${suit.name}_1.png`) });
-            this.#cardList.push({name: `${initial}2`, obj: new Card(suit.obj, "2",  `${initial}2`, 10, `${imageFolderPath}/${suit.name}/card_${suit.name}_2.png`) });
-            this.#cardList.push({name: `${initial}J`, obj: new Card(suit.obj, "J",  `${initial}J`, 11, `${imageFolderPath}/${suit.name}/card_${suit.name}_11.png`)});
-            this.#cardList.push({name: `${initial}Q`, obj: new Card(suit.obj, "Q",  `${initial}Q`, 12, `${imageFolderPath}/${suit.name}/card_${suit.name}_12.png`)});
-            this.#cardList.push({name: `${initial}K`, obj: new Card(suit.obj, "K",  `${initial}K`, 13, `${imageFolderPath}/${suit.name}/card_${suit.name}_13.png`)});
+            this.#cardList.push({name: `${initial}J`, obj: new Card(suit.obj, "J",  `${initial}J`, 9,  `${imageFolderPath}/${suit.name}/card_${suit.name}_11.png`)});
+            this.#cardList.push({name: `${initial}Q`, obj: new Card(suit.obj, "Q",  `${initial}Q`, 10, `${imageFolderPath}/${suit.name}/card_${suit.name}_12.png`)});
+            this.#cardList.push({name: `${initial}K`, obj: new Card(suit.obj, "K",  `${initial}K`, 11, `${imageFolderPath}/${suit.name}/card_${suit.name}_13.png`)});
+            this.#cardList.push({name: `${initial}1`, obj: new Card(suit.obj, "1",  `${initial}1`, 12, `${imageFolderPath}/${suit.name}/card_${suit.name}_1.png`) });
+            this.#cardList.push({name: `${initial}2`, obj: new Card(suit.obj, "2",  `${initial}2`, 13, `${imageFolderPath}/${suit.name}/card_${suit.name}_2.png`) });
         }
         this.#cardList.push({name: "Joker1", obj: new Joker("Joker1",  99,  `${imageFolderPath}/card_joker.png`)});
         this.#cardList.push({name: "Joker2", obj: new Joker("Joker2",  99,  `${imageFolderPath}/card_joker.png`)});
