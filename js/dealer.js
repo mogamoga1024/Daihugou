6455c5655a2f9ff08e88d76a0339483b9f0fed82
mogamoga <mogamoga.g1thu6@gmail.com>
2022-05-30 09:20:28 +0900

    カードを配る

---
+++ js/dealer.js
@@ -0,0 +1,39 @@
+
+class Dearler {
+    static dealCardList(playerList) {
+        playerList.forEach(p => p.cardList = []);
+
+        const allCardList = CardFactory.createCardList();
+        const allCardListIndexs = [];
+        for (let i = 0; i < allCardList.length; i++) {
+            allCardListIndexs.push(i);
+        }
+
+        // カードのシャッフル
+        for (let i = 0; i < allCardListIndexs.length; i++) {
+            const j = Common.randomInt(allCardListIndexs.length);
+            const tmpIndex = allCardListIndexs[i];
+            allCardListIndexs[i] = allCardListIndexs[j];
+            allCardListIndexs[j] = tmpIndex;
+        }
+
+        // プレイヤーのシャッフル（端数の振り分けをランダムにするため）
+        for (let i = 0; i < playerList.length; i++) {
+            const j = Common.randomInt(playerList.length);
+            const tmpPlayer = playerList[i];
+            playerList[i] = playerList[j];
+            playerList[j] = tmpPlayer;
+        }
+
+        // カードを配る
+        let playerIndex = 0;
+        for (const index of allCardListIndexs) {
+            let player = playerList[playerIndex];
+            playerIndex = (playerIndex + 1) % playerList.length;
+
+            player.cardList.push(allCardList[index]);
+        }
+
+        playerList.forEach(p => Common.sortcardList(p.cardList));
+    }
+}
