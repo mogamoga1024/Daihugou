a03276274042eaf84eaeb8767905366732429b8e
mogamoga <mogamoga.g1thu6@gmail.com>
2022-06-01 02:16:54 +0900

    vue router

---
+++ js/components/gameComponent.vue
@@ -0,0 +1,4 @@
+
+<template src="./js/components/template/game.html"></template>
+<style scoped src="./js/components/style/game.css"></style>
+<script src="./js/components/script/game.js"></script>
---
+++ js/components/script/game.js
@@ -0,0 +1,60 @@
+
+module.exports = {
+    data() {
+        return {
+            player: new Player("YOU"),
+            anotherPlayerList: [
+                new Player("CPU1"),
+                new Player("CPU2"),
+                new Player("CPU3"),
+            ],
+            battleFieldCardList: [
+                // CardFactory.createCard("s1"),
+                // CardFactory.createCard("s2"),
+                // CardFactory.createCard("dK"),
+                // CardFactory.createCard("s1"),
+                // CardFactory.createCard("s2"),
+            ]
+        }
+    },
+    created() {
+        Dearler.dealCardList([this.player, ...this.anotherPlayerList]);
+    },
+    mounted() {
+
+    },
+    computed: {
+        battleFieldCardContainerWidth() {
+            // 値はテキトー
+            return Math.min(100 + 80 * (this.battleFieldCardList.length - 1), 800);
+        }
+    },
+    methods: {
+        cardClick(index) {
+            const card = this.player.cardList[index];
+            card.isSelected = !card.isSelected;
+        },
+        selectedCardReset() {
+            this.player.cardList.forEach(c => {
+                c.isSelected = false;
+            });
+        },
+        outputCardList() {
+            const selectedCardList = [];
+            const tmpCardList = [];
+
+            for (const card of this.player.cardList) {
+                if (card.isSelected) {
+                    card.isSelected = false;
+                    selectedCardList.push(card);
+                }
+                else {
+                    tmpCardList.push(card);
+                }
+            }
+
+            this.battleFieldCardList = selectedCardList;
+            this.player.cardList = tmpCardList;
+        }
+    }
+};
