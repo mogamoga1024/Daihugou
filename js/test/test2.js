c3bbb5354f8633fce60040cd203adbc89921f30b
mogamoga <mogamoga.g1thu6@gmail.com>
2022-09-16 22:20:24 +0900

    test 革命考慮 red

--- js/test/test2.js
+++ js/test/test2.js
@@ -10,67 +10,73 @@
 
     // memo: ((s|c|d|h)([1-9]|T|J|Q|K))|Joker1|Joker2
 
-    // TODO Jokerは後々何でも変換できるようになるので、その時は修正する
-    test("思考", function() {
-        cpu.cardList = CardFactory.createCardList("s3, d8, Joker1");
-
-        strictEqual(Common.cardListToString(cpu.outputHand([])), "d8");
-        strictEqual(Common.cardListToString(cpu.outputHand([])), "Joker1");
-        strictEqual(Common.cardListToString(cpu.outputHand([])), "s3");
-    });
-
     test("思考 single", function() {
-        cpu.cardList = CardFactory.createCardList("s3, d8, s2");
+        cpu.cardList = CardFactory.createCardList("s3, d7, s2");
         
         CardFactory.getCard("Joker1").isDead = true;
         CardFactory.getCard("Joker2").isDead = true;
 
-        strictEqual(Common.cardListToString(cpu.outputHand([])), "d8");
+        strictEqual(Common.cardListToString(cpu.outputHand([])), "d7");
         strictEqual(Common.cardListToString(cpu.outputHand([])), "s2");
         strictEqual(Common.cardListToString(cpu.outputHand([])), "s3");
     });
 
     test("思考 single multi", function() {
-        cpu.cardList = CardFactory.createCardList("s3, d8, s9, d9, s2");
+        cpu.cardList = CardFactory.createCardList("s3, d7, sT, dT, s2");
         
         CardFactory.getCard("Joker1").isDead = true;
         CardFactory.getCard("Joker2").isDead = true;
 
-        strictEqual(Common.cardListToString(cpu.outputHand([])), "d8");
-        strictEqual(Common.cardListToString(cpu.outputHand([])), "s9, d9");
+        strictEqual(Common.cardListToString(cpu.outputHand([])), "d7");
+        strictEqual(Common.cardListToString(cpu.outputHand([])), "sT, dT");
         strictEqual(Common.cardListToString(cpu.outputHand([])), "s2");
         strictEqual(Common.cardListToString(cpu.outputHand([])), "s3");
     });
 
     test("思考 single multi stairs", function() {
-        cpu.cardList = CardFactory.createCardList("s3, s4, c4, h5, h6, h7, d8, s9, d9, hT, hJ, hQ, hK, s2");
+        cpu.cardList = CardFactory.createCardList("s3, s4, c4, h5, h6, h7, d9, sT, dT, hJ, hQ, hK, h1, s2");
         
         CardFactory.getCard("Joker1").isDead = true;
         CardFactory.getCard("Joker2").isDead = true;
 
-        strictEqual(Common.cardListToString(cpu.outputHand([])), "d8");
+        strictEqual(Common.cardListToString(cpu.outputHand([])), "d9");
         strictEqual(Common.cardListToString(cpu.outputHand([])), "s4, c4");
-        strictEqual(Common.cardListToString(cpu.outputHand([])), "s9, d9");
+        strictEqual(Common.cardListToString(cpu.outputHand([])), "sT, dT");
         strictEqual(Common.cardListToString(cpu.outputHand([])), "h5, h6, h7");
-        strictEqual(Common.cardListToString(cpu.outputHand([])), "hT, hJ, hQ, hK");
+        strictEqual(Common.cardListToString(cpu.outputHand([])), "hJ, hQ, hK, h1");
         strictEqual(Common.cardListToString(cpu.outputHand([])), "s2");
         strictEqual(Common.cardListToString(cpu.outputHand([])), "s3");
     });
 
     test("思考 革命中 single multi stairs", function() {
-        cpu.cardList = CardFactory.createCardList("s3, s4, c4, h5, h6, h7, d8, s9, d9, hT, hJ, hQ, hK, s2");
+        cpu.cardList = CardFactory.createCardList("s3, s4, c4, h5, h6, h7, d9, sT, dT, hJ, hQ, hK, h1, s2");
         
         CardFactory.getCard("Joker1").isDead = true;
         CardFactory.getCard("Joker2").isDead = true;
 
         GameManager.revolution();
 
-        strictEqual(Common.cardListToString(cpu.outputHand([])), "d8");
-        strictEqual(Common.cardListToString(cpu.outputHand([])), "s9, d9");
+        strictEqual(Common.cardListToString(cpu.outputHand([])), "d9");
+        strictEqual(Common.cardListToString(cpu.outputHand([])), "sT, dT");
         strictEqual(Common.cardListToString(cpu.outputHand([])), "s4, c4");
-        strictEqual(Common.cardListToString(cpu.outputHand([])), "hK, hQ, hJ, hT");
+        strictEqual(Common.cardListToString(cpu.outputHand([])), "h1, hK, hQ, hJ");
         strictEqual(Common.cardListToString(cpu.outputHand([])), "h7, h6, h5");
         strictEqual(Common.cardListToString(cpu.outputHand([])), "s3");
         strictEqual(Common.cardListToString(cpu.outputHand([])), "s2");
+    });
+
+    test("思考 革命考慮", function() {
+        cpu.cardList = CardFactory.createCardList("s3, s4, c4, d4, h4, s9, s2");
+        
+        CardFactory.getCard("Joker1").isDead = true;
+        CardFactory.getCard("Joker2").isDead = true;
+
+        strictEqual(Common.cardListToString(cpu.outputHand([])), "s2");
+        strictEqual(Common.cardListToString(cpu.outputHand([])), "s4, c4, d4, h4");
+
+        GameManager.revolution();
+
+        strictEqual(Common.cardListToString(cpu.outputHand([])), "s3");
+        strictEqual(Common.cardListToString(cpu.outputHand([])), "s9");
     });
 })();
