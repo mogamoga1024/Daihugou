65d99da0171ee5361d9af3ef69c16a4393c98c63
mogamoga <mogamoga.g1thu6@gmail.com>
2022-09-19 18:00:49 +0900

    single test 応手

--- js/test/test3.js
+++ js/test/test3.js
@@ -2,18 +2,14 @@
 (function() {
     let cpu = null;
     let singleThinking = null;
-    let multiThinking = null;
-    let stairsThinking = null;
     
     CardFactory.initCardList();
     const twoPower = CardFactory.getCard("s2").power;
     
-    module("CPUのAIのテスト3（Thinkingクラス系）", {
+    module("CPUのAIのテスト3（SingleThinkingクラス）", {
         setup() {
             cpu = new Cpu("CPU1");
             singleThinking = null;
-            multiThinking = null;
-            stairsThinking = null;
             CardFactory.initCardList();
         }
     });
@@ -60,7 +56,60 @@
         );
     });
 
-    // TODO
+    test("思考 応手 single 残り2役 最強なし", function() {
+        const d3 = CardFactory.getCard("d3");
+        const s7 = CardFactory.getCard("s7");
+        const s1 = CardFactory.getCard("s1");
+        singleThinking = new SingleThinking([[s7], [s1]]);
 
+        strictEqual(Common.cardListToString(
+            singleThinking.outputHandIfHandInBattleField([d3], 2, twoPower)), "s7"
+        );
+    });
 
+    test("思考 応手 single 残り3役", function() {
+        const d3 = CardFactory.getCard("d3");
+        const s7 = CardFactory.getCard("s7");
+        const s9 = CardFactory.getCard("s9");
+        const s2 = CardFactory.getCard("s2");
+        singleThinking = new SingleThinking([[s7], [s9], [s2]]);
+
+        strictEqual(Common.cardListToString(
+            singleThinking.outputHandIfHandInBattleField([d3], 3, twoPower)), "s7"
+        );
+    });
+
+    test("思考 応手 single 残り3役", function() {
+        const d7 = CardFactory.getCard("d7");
+        const s7 = CardFactory.getCard("s7");
+        const s9 = CardFactory.getCard("s9");
+        const s2 = CardFactory.getCard("s2");
+        singleThinking = new SingleThinking([[s7], [s9], [s2]]);
+
+        strictEqual(Common.cardListToString(
+            singleThinking.outputHandIfHandInBattleField([d7], 3, twoPower)), "s9"
+        );
+    });
+
+    test("思考 応手 single 残り3役", function() {
+        const d1 = CardFactory.getCard("d1");
+        const s7 = CardFactory.getCard("s7");
+        const s9 = CardFactory.getCard("s9");
+        const s2 = CardFactory.getCard("s2");
+        singleThinking = new SingleThinking([[s7], [s9], [s2]]);
+
+        strictEqual(Common.cardListToString(
+            singleThinking.outputHandIfHandInBattleField([d1], 3, twoPower)), ""
+        );
+    });
+
+    test("思考 応手 single 残り3役", function() {
+        const d1 = CardFactory.getCard("d1");
+        const s2 = CardFactory.getCard("s2");
+        singleThinking = new SingleThinking([[s2]]);
+
+        strictEqual(Common.cardListToString(
+            singleThinking.outputHandIfHandInBattleField([d1], 3, twoPower)), ""
+        );
+    });
 })();
