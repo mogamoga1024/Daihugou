9b6c1123cc0f795df8a529c68bdee213cbb8dc62
mogamoga <mogamoga.g1thu6@gmail.com>
2022-09-19 15:28:50 +0900

    thinking multi 応手

--- js/player/cpu.js
+++ js/player/cpu.js
@@ -160,35 +160,7 @@ class Cpu extends Player {
                     break;
 
                 case Hand.Multi:
-                    const tmpMultiHandList = this._multiHandList.filter(h =>
-                        h.length === battleFieldHand.length &&
-                        h[0].power > battleFieldHand[0].power
-                    );
-                    if (tmpMultiHandList.length === 0) {
-                        selectedHand = []; // TODO 考えもの
-                        break;
-                    }
-
-                    if (
-                        handCount === 2 &&
-                        tmpMultiHandList.last()[0].power === strongestCardPower
-                    ) {
-                        // 2役で最強の役を持っている場合、最強の役をだす
-                        selectedHand = tmpMultiHandList.last();
-                    }
-                    else {
-                        // 2役以外 または 2役で最強の役を持っていない場合
-
-                        const hand = tmpMultiHandList[0]
-                        if (hand[0].power === strongestCardPower) {
-                            // 最強の役しかない場合、応手しない
-                            selectedHand = [];
-                        }
-                        else {
-                            // 弱い役で応手する
-                            selectedHand = hand;
-                        }
-                    }
+                    selectedHand = this._multiThinking.outputHandIfHandInBattleField(battleFieldHand);
                     break;
 
                 case Hand.Stairs:
