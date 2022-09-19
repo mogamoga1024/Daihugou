b93e218ea8911776c351ea6205b1dee57202dffa
mogamoga <mogamoga.g1thu6@gmail.com>
2022-09-19 16:49:56 +0900

    test single 応手

--- js/test/test3.js
+++ js/test/test3.js
@@ -4,7 +4,10 @@
     let singleThinking = null;
     let multiThinking = null;
     let stairsThinking = null;
-
+    
+    CardFactory.initCardList();
+    const twoPower = CardFactory.getCard("s2").power;
+    
     module("CPUのAIのテスト3（Thinkingクラス系）", {
         setup() {
             cpu = new Cpu("CPU1");
@@ -21,7 +24,9 @@
         const s3 = CardFactory.getCard("s3");
         singleThinking = new SingleThinking([]);
 
-        strictEqual(Common.cardListToString(singleThinking.outputHandIfHandInBattleField([s3])), "");
+        strictEqual(Common.cardListToString(
+            singleThinking.outputHandIfHandInBattleField([s3], 8, twoPower)), ""
+        );
     });
 
     test("思考 応手 single 役なし", function() {
@@ -29,14 +34,34 @@
         const s1 = CardFactory.getCard("s1");
         singleThinking = new SingleThinking([s7]);
 
-        strictEqual(Common.cardListToString(singleThinking.outputHandIfHandInBattleField([s1])), "");
+        strictEqual(Common.cardListToString(
+            singleThinking.outputHandIfHandInBattleField([s1], 8, twoPower)), ""
+        );
     });
 
-    // test("思考 応手 single 残り2役 最強あり", function() {
-    //     singleThinking = new SingleThinking(["s4"]);
+    test("思考 応手 single 残り1役", function() {
+        const d3 = CardFactory.getCard("d3");
+        const s7 = CardFactory.getCard("s7");
+        singleThinking = new SingleThinking([s7]);
 
-    //     strictEqual(Common.cardListToString(singleThinking.outputHandIfHandInBattleField(["s3"])), "");
-    // });
+        strictEqual(Common.cardListToString(
+            singleThinking.outputHandIfHandInBattleField([d3], 1, twoPower)), "s7"
+        );
+    });
+
+    test("思考 応手 single 残り2役 最強あり", function() {
+        const d3 = CardFactory.getCard("d3");
+        const s7 = CardFactory.getCard("s7");
+        const s2 = CardFactory.getCard("s2");
+        singleThinking = new SingleThinking([s7, s2]);
+
+        CardFactory.getCard("Joker1").isDead = true;
+        CardFactory.getCard("Joker2").isDead = true;
+
+        strictEqual(Common.cardListToString(
+            singleThinking.outputHandIfHandInBattleField([d3], 2, twoPower)), ""
+        );
+    });
 
 
 
